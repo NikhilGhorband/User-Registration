8bf902b8d187ddcf8f5a3e2288f0cc6e0a10ad53
NikhilGhorband <127277360+NikhilGhorband@users.noreply.github.com>
2023-06-30 23:49:32 +0530

    UC1 Valid First Name

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UC1_ValidFirstName
+{
+    
+    public class UserRegistrationValidation
+    {
+        private static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+        }
+    }
+}
+
